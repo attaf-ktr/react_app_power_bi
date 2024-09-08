be51b3bbd609ef5ad85087b9be960f9c1b4fd1a4
attaf-ktr <kaoutrb@gmail.com>
2024-09-08 21:31:34 +0100

    .

--- react-admin/src/scenes/global/Sidebar.jsx
+++ react-admin/src/scenes/global/Sidebar.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import {Menu, MenuItem } from "react-pro-sidebar";
-import { Sidebar as ProSidebar } from 'react-pro-sidebar';
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
-
+import "react-pro-sidebar/dist/css/styles.css";
 import { tokens } from "../../theme";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
