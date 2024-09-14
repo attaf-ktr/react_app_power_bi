27abd978b522fbfa9454749b2a34f89b9d7829e2
attaf-ktr <kaoutrb@gmail.com>
2024-09-14 21:10:26 +0100

    connction to sales.sql  & create function getcustomers

--- backend/database.js
+++ backend/database.js
@@ -3,6 +3,13 @@ import mysql from 'mysql2'
 const pool = mysql.createPool({ //a pool is a collection of connections to the darabase ; 
     host: '127.0.0.1',
     user: 'root',
-    password: '',
+    password: '1234sql@',
     database: 'sales'
 }).promise() // to use the promise api version 
+
+export async function getcustomers() {
+    const [rows] = await pool.query("SELECT * FROM customers")
+    return rows
+  }
+  const customers = await getcustomers()
+  console.log(customers)
